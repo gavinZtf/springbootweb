713ee73a9d98a0c7236001bc3c348cca3df93747
gavin <2605170374@qq.com>
2018-07-09 23:17:20 +0800

    测试案例完善

--- src/main/java/com/gavin/springbootweb/web/ch4_6/TestControllerIntegrationTests.java
+++ src/main/java/com/gavin/springbootweb/web/ch4_6/TestControllerIntegrationTests.java
@@ -1,6 +1,7 @@
 package com.gavin.springbootweb.web.ch4_6;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockHttpServletRequest;
@@ -12,6 +13,13 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
 import com.gavin.springbootweb.MyMvcConfig;
 import com.gavin.springbootweb.service.DemoService;
 
@@ -20,7 +28,7 @@ import com.gavin.springbootweb.service.DemoService;
  * @date 2018年7月9日
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = MyMvcConfig.class)
+@ContextConfiguration(classes = {MyMvcConfig.class})
 @WebAppConfiguration("src/main/resources")
 public class TestControllerIntegrationTests {
 
@@ -41,5 +49,22 @@ public class TestControllerIntegrationTests {
     @Before
     public void setUp(){
         this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+    
+    @Test
+    public void testNormalController() throws Exception{
+    	mockMvc.perform(get("/normal"))
+    			.andExpect(status().isOk())
+    			.andExpect(view().name("page"))
+    	.andExpect(forwardedUrl("/WEB-INF/classes/views/page.jsp"))
+    	.andExpect(model().attribute("msg", demoService.saySomething()));
+    }
+    
+    @Test
+    public void testRestController() throws Exception{
+    	mockMvc.perform(get("/testRest"))
+    	.andExpect(status().isOk())
+    	.andExpect(content().contentType("text/plain;charset=UTF-8"))
+    	.andExpect(content().string(demoService.saySomething()));
     }
 }
